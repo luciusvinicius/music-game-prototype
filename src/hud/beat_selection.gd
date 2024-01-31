2034d3ea6de266704127c64da1d25b6c4ab972c4
Lucius Vinicius <luciusviniciusf@ua.pt>
2024-01-31 05:09:11 +0000

    closes #15

--- src/hud/beat_selection.gd
+++ src/hud/beat_selection.gd
@@ -1,10 +1,12 @@
 extends Control
 
 @onready var beat_manager = $Beat
-@onready var bpm : Label = $PanelContainer/VBoxContainer/BPM
+@onready var bpm : Label = $PanelContainer/VBoxContainer/BPMContainer/BPM
 @onready var beats = $PanelContainer/VBoxContainer/Beats
-@onready var melodies_loop = $PanelContainer/VBoxContainer/MelodiesLoop
+@onready var melodies_loop = $PanelContainer/VBoxContainer/MeasuresContainer/MelodiesLoop
 
+var melodies_looped := 0
+const UIButton = preload("res://src/hud/UIButton.gd")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Create Beats buttons
@@ -12,6 +14,7 @@ func _ready():
 		var beat_button = Button.new()
 		beat_button.text = beat.name
 		beat_button.pressed.connect(_on_beat_button_pressed.bind(beat.name)) # Add "beat.name" as an extra argument
+		beat_button.set_script(UIButton)
 		beats.add_child(beat_button)
 
 func _on_beat_button_pressed(beat_name: String):
@@ -20,13 +23,20 @@ func _on_beat_button_pressed(beat_name: String):
 func _on_beat_1_pressed():
 	beat_manager.play("")
 
-func _on_bpm_slider_value_changed(value):
+func _set_bpm(value):
 	beat_manager.bpm = value
-	bpm.text = "BPM: %d" % value
+	bpm.text = " BPM: %d " % value
+
+func _set_melodies_loop(value):
+	SignalManager.melodies_in_loop_changed.emit(value)
+	melodies_loop.text = " Measures: %d " % value
 
 func _on_no_beat_pressed():
 	beat_manager.stop()
 
-func _on_melodies_loop_slide_value_changed(value):
-	SignalManager.melodies_in_loop_changed.emit(value)
-	melodies_loop.text = "Measures Looped: %d" % value
+func _on_bpm_pressed(modifier):
+	_set_bpm(clamp(beat_manager.bpm + modifier, 1, 300))
+
+func _on_melodies_pressed(modifier):
+	melodies_looped = clamp(melodies_looped + modifier, 0, 4)
+	_set_melodies_loop(melodies_looped)
--- src/hud/song_selection.gd
+++ src/hud/song_selection.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var song_player = $SongPlayer
 @onready var songs = $PanelContainer/VBoxContainer/Songs
+const UIButton = preload("res://src/hud/UIButton.gd")
 
 func _ready():
 	SignalManager.unlock_next_song.connect(unlock_song)
@@ -13,6 +14,7 @@ func unlock_song(song_name:String):
 	var songButton = Button.new()
 	songButton.text = song_name
 	songButton.pressed.connect(play_song.bind(song_name))
+	songButton.set_script(UIButton)
 	songs.add_child(songButton)
 
 	
